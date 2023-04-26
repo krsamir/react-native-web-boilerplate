54b72011fd9c0ed253b63ea335ceb408acaf86ce
Samir Kumar <503356832@ge.com>
2023-04-26 15:55:31 +0530

    added react-native-paper support

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Platform} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {Button} from 'react-native-paper';
 
 function App() {
   return (
-    <View>
-      <Text>React Native</Text>App
-    </View>
+    <PaperProvider>
+      {Platform.OS === 'web' ? (
+        <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialCommunityIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
+        }
+      `}</style>
+      ) : null}
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <View style={{width: 200, margin: 20}}>
+        <Text>React Native</Text>
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+          Button
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const rootDir = path.join(__dirname, '..');
 const webpackEnv = process.env.NODE_ENV || 'development';
-
+require('core-js');
 const imageLoaderConfiguration = {
   test: /\.(gif|jpe?g|png|svg)$/,
   use: {
@@ -14,8 +14,36 @@ const imageLoaderConfiguration = {
     },
   },
 };
+const configurationForRNP = {
+  test: /\.js$/,
+  // exclude: /node_modules[/\\](?!react-native-vector-icons)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      // Disable reading babel configuration
+      babelrc: false,
+      configFile: false,
+
+      // The configuration for compilation
+      presets: [
+        ['@babel/preset-env', {useBuiltIns: 'entry', corejs: '3.30.1'}],
+        '@babel/preset-react',
+        '@babel/preset-flow',
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+      ],
+    },
+  },
+};
 
 module.exports = {
+  devServer: {
+    port: 3000,
+    open: true,
+  },
   mode: webpackEnv,
   entry: {
     app: path.join(rootDir, './index.web.ts'),
@@ -27,12 +55,25 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
+      configurationForRNP,
       {
         test: /\.(tsx|ts|jsx|js|mjs)$/,
         exclude: /node_modules/,
         loader: 'ts-loader',
       },
+      {
+        test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+        type: 'asset/resource',
+      },
       imageLoaderConfiguration,
+      {
+        test: /\.ttf$/,
+        loader: 'url-loader', // or directly file-loader
+        include: path.resolve(
+          __dirname,
+          'node_modules/react-native-vector-icons',
+        ),
+      },
     ],
   },
   plugins: [
@@ -52,8 +93,11 @@ module.exports = {
       '.jsx',
       '.js',
     ], // read files in fillowing order
-    alias: Object.assign({
-      'react-native$': 'react-native-web',
-    }),
+    // alias: Object.assign({
+    //   'react-native$': 'react-native-web',
+    // }),
+    alias: {
+      'react-native$': require.resolve('react-native-web'),
+    },
   },
 };
